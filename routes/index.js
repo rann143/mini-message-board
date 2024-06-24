cba7604b3c406fd41b6b09d924b42175dffaea9a
Daniel <roderm99@gmail.com>
2024-06-24 08:52:16 -0400

    first commit;completed 1-6 from TOP assignment

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date(),
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date(),
+    },
+  ];
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+module.exports = router;
