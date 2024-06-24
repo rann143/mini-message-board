9bb02a342039488efa3e1c0e1f42ee5bdbda7cb2
Daniel <roderm99@gmail.com>
2024-06-24 10:50:51 -0400

    Create routes and pug templates; completed basic version

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,33 @@
 var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date(),
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date(),
-    },
-  ];
   res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "My Message" });
+});
+
+router.post("/new", function (req, res, next) {
+  const messageUser = req.body.authorInput;
+  const messageText = req.body.messageInput;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
